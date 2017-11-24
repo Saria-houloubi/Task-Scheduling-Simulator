593bb251de4479eb0f707b5502a12b819d6dc616
Saria houloubi <houloubis@gmail.com>
2017-11-24 13:17:32 +0200

    Added the instruction menu and the instruction items but with out implementing the list control for the instructions

---
+++ Project/ParallelPro/TishreenUniversity.ParallelPro/Controls/Main Window Buttons/InstructionMenuControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TishreenUniversity.ParallelPro.Controls
+{
+    /// <summary>
+    /// Interaction logic for InstructionMenuControl.xaml
+    /// </summary>
+    public partial class InstructionMenuControl : BaseUserControl
+    {
+        public InstructionMenuControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
