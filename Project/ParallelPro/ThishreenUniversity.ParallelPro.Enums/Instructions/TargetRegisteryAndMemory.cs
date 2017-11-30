0c1eaac537c7b193f9b3c364b9d9e097906094c8
Saria houloubi <houloubis@gmail.com>
2017-11-30 11:00:26 +0200

    Added enum classes to share across the application to slandered the names of the instructions and registries

---
+++ Project/ParallelPro/ThishreenUniversity.ParallelPro.Enums/Instructions/TargetRegisteryAndMemory.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThishreenUniversity.ParallelPro.Enums.Instructions
+{
+    /// <summary>
+    /// Holds all the regiteries and memory spots that user can only use in the application
+    /// Value = 0 ===> it is registery
+    /// Value = 1 ===> it is a memory spot
+    /// </summary>
+    public enum RegisteriesAndMemory
+    {
+
+        /// <summary>
+        /// Registeries that will be used 
+        /// </summary>
+        F1 = 0,
+        F2 = 0,
+        F3 = 0,
+        F4 = 0,
+        F5 = 0,
+        F6 = 0,
+        F7 = 0,
+        F8 = 0,
+        F9 = 0,
+        F10 = 0,
+        F11 = 0,
+        F12 = 0,
+        F14 = 0,
+        F15 = 0,
+        F16 = 0,
+        F17 = 0,
+        F18 = 0,
+        F19 = 0,
+        F20 = 0,
+        F21 = 0,
+        F22 = 0,
+        F23 = 0,
+        F24 = 0,
+        F25 = 0,
+        F26 = 0,
+        F27 = 0,
+        F28 = 0,
+        F29 = 0,
+        F30 = 0,
+        F31 = 0,
+
+        /// <summary>
+        /// Memory Spots the will be used
+        /// </summary>
+        M1 = 1,
+        M2 = 1,
+        M3 = 1,
+        M4 = 1,
+        M5 = 1,
+        M6 = 1,
+        M7 = 1,
+        M8 = 1,
+        M9 = 1,
+        M10 = 1,
+        M11 = 1,
+        M12 = 1,
+        M14 = 1,
+        M15 = 1,
+        M16 = 1,
+        M17 = 1,
+        M18 = 1,
+        M19 = 1,
+        M20 = 1,
+        M21 = 1,
+        M22 = 1,
+        M23 = 1,
+        M24 = 1,
+        M25 = 1,
+        M26 = 1,
+        M27 = 1,
+        M28 = 1,
+        M29 = 1,
+        M30 = 1,
+        M31 = 1,
+
+    }
+}
