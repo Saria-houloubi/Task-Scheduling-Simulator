6df0f5552076b141eb982de875e48910e67d6863
Saria Houloubi <houloubis@gmail.com>
2019-08-29 00:36:07 +0300

    Added word dictionery

---
+++ Project V2.0/ParallelProcessersSimulator/PPS.UI.Shared/Views/Base/BaseWindow.cs
@@ -0,0 +1,30 @@
+﻿using MahApps.Metro.Controls;
+
+namespace PPS.UI.Shared.Views.Base
+{
+    /// <summary>
+    /// The base window for cross functions
+    /// </summary>
+    public class BaseWindow : MetroWindow
+    {
+
+        #region Properties
+
+        #endregion
+
+        #region Constructer
+        /// <summary>
+        /// Defauit contructer
+        /// </summary>
+        public BaseWindow()
+        {
+
+
+            //Start in the center and as max size
+            this.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterOwner;
+            this.WindowState = System.Windows.WindowState.Maximized;
+        }
+        #endregion
+
+    }
+}
