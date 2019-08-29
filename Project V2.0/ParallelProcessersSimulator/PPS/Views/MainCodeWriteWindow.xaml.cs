fd8136ac60b10da340387f6e836e9e0aa3e958f1
Saria Houloubi <houloubis@gmail.com>
2019-08-29 19:55:34 +0300

    Done main entry window

---
+++ Project V2.0/ParallelProcessersSimulator/PPS/Views/MainCodeWriteWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using PPS.UI.Shared.Views.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PPS.Views
+{
+    /// <summary>
+    /// Interaction logic for MainCodeWriteWindow.xaml
+    /// </summary>
+    public partial class MainCodeWriteWindow : BaseWindow
+    {
+        public MainCodeWriteWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- Project V2.0/ParallelProcessersSimulator/PPS/Views/MainWindow.xaml.cs
+++ Project V2.0/ParallelProcessersSimulator/PPS/Views/MainWindow.xaml.cs
@@ -11,5 +11,22 @@ namespace PPS.Views
         {
             InitializeComponent();
         }
+
+        /// <summary>
+        /// The function to open the main code write window
+        /// the only place allowed to be attached to the view 
+        /// all other functions must be as commands
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OpenMain_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            //Create the window object
+            MainCodeWriteWindow mainCodeWriteWindow = new MainCodeWriteWindow();
+            //Show the window to the user
+            mainCodeWriteWindow.Show();
+            //Close this window
+            this.Close();
+        }
     }
 }
